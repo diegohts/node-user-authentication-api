08dc6732de0a09748061e76c217cb0c5c9e95d99
Diego Henrique <diegohrp20@gmail.com>
2022-02-09 04:58:11 -0300

    Desenvolvendo a inserção de usuários

--- live-02/src/repositories/user.repository.ts
+++ live-02/src/repositories/user.repository.ts
@@ -31,6 +31,23 @@ class UserRepository {
         return user;
     }
 
+    async create(user: User): Promise<string> {
+        const query = `
+            INSERT INTO application_user (
+                username,
+                password
+            )
+            VALUES ($1, crypt($2, 'my_salt'))
+            RETURNING uuid
+        `;
+
+        const values = [user.username, user.password];
+
+        const { rows } = await db.query<{ uuid: string }>(query, values);
+        const [newUser] = rows;
+        return newUser.uuid;
+    }
+
 }
 
 export default new UserRepository(); 
