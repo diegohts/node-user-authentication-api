a9791c155277dd56144135b15635a427e9f3ade0
Diego Henrique <diegohrp20@gmail.com>
2022-02-10 04:17:23 -0300

    Adicionando o middleware de autenticação Bearer no arquivo index, possibilitando ou não acesso as rotas de usuários

--- live-03/src/middlewares/bearer-authentication.middleware.ts
+++ live-03/src/middlewares/bearer-authentication.middleware.ts
@@ -1,7 +1,6 @@
 import { NextFunction, Response ,Request } from "express";
 import ForbiddenError from "../models/errors/forbidden.error.model";
 import JWT from 'jsonwebtoken';
-import userRepository from "../repositories/user.repository";
 
 async function bearerAuthenticationMiddleware(req: Request, res: Response, next: NextFunction){
 
@@ -19,10 +18,6 @@ async function bearerAuthenticationMiddleware(req: Request, res: Response, next:
             throw new ForbiddenError('Tipo de authenticação inválido');
         }
 
-        const tokenContent = Buffer.from(token, 'base64').toString('utf-8');
-
-        const [username, password] = tokenContent.split(':');
-
         const tokenPayload = JWT.verify(token, 'temy_secret_key');
 
         if(typeof tokenPayload !== 'object' || !tokenPayload.sub){
@@ -30,11 +25,7 @@ async function bearerAuthenticationMiddleware(req: Request, res: Response, next:
             
         }
 
-        const uuid = tokenPayload.sub;
-        const user = {
-            uuid: tokenPayload.sub,
-            username: tokenPayload.username
-        };
+        const user = { uuid: tokenPayload.sub, username: tokenPayload.username };
 
         req.user = user;
         next();
